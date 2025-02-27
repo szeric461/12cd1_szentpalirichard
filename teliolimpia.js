5b7adedf34283f358f6ecee083742a9a0771139e
szeric461 <93657723+szeric461@users.noreply.github.com>
2025-02-27 09:15:28 +0100

    GET hiba javítása
    app.get("/" , (req, res) => {
        res.send("Működik a szerver.")
    })

--- teliolimpia.js
+++ teliolimpia.js
@@ -5,6 +5,10 @@ const bodyparser = require("body-parser")
 
 app.use(bodyparser.json())
 
+app.get("/" , (req, res) => {
+    res.send("Működik a szerver.")
+})
+
 app.listen(3000, () => {
     console.log("A téliolimpia szervere a 3000-es porton fut.")
 })
