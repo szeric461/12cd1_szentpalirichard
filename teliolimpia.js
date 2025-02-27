0f657e63b6924cd7611956e138ba0faefd033574
szeric461 <93657723+szeric461@users.noreply.github.com>
2025-02-27 09:25:59 +0100

    db kapcsolat létrehozása

--- teliolimpia.js
+++ teliolimpia.js
@@ -1,9 +1,21 @@
 const express = require("express")
 const app = express()
 const bodyparser = require("body-parser")
+const cors = require("cors")
+const mysql = require("mysql")
 
-
+app.use(cors())
 app.use(bodyparser.json())
+
+const db = mysql.createConnection(
+    {
+    user: "root",
+    host: "localhost",
+    port: 3307,
+    password: "",
+    database: "teliolimpia"
+    }
+)
 
 app.get("/" , (req, res) => {
     res.send("Működik a szerver.")
